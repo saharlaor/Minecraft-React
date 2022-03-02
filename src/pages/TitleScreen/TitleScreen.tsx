cbfffaa03d646533f99e5d6a7e5887b12f425c1b
saharoori <saharlaor@gmail.com>
2022-03-02 20:10:55 +0200

    Add title screen with title

--- src/pages/TitleScreen/TitleScreen.tsx
+++ src/pages/TitleScreen/TitleScreen.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Button from "../../components/Button/Button";
+import "./_TitleScreen.scss";
 
-export type TitlescreenProps = {}
+export type TitleScreenProps = {}
 
-export default function Titlescreen({}: TitlescreenProps) {
-    return (<div className="TitleScreen"/>);
+export default function Titlescreen({}: TitleScreenProps) {
+    return (<div className="TitleScreen">
+        <h1 className="TitleScreen__title">Minecraft</h1>
+    </div>);
 }
